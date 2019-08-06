b6fbf33829820492a9ed80e41b0aa47a0c593d7f
Jason <jasonfchan@gmail.com>
2019-08-06 19:32:39 -0400

    Add files via upload

--- profiling_code/profileweather.scala
+++ profiling_code/profileweather.scala
@@ -1,4 +1,4 @@
-val weatherPath: String = "hdfs:///user/jwc516/weather/1803943.csv"
+val weatherPath: String = "hdfs:///user/rag551/weather/1818263.csv"
 
 val weatherData = sc.textFile(weatherPath)
 
@@ -9,26 +9,26 @@ val splitData = filteredRDD.map(line=> line.split(",(?=(?:[^\"]*\"[^\"]*\")*[^\"
 
 //Check if specific values can be parsed to numeric
 //Check if Date values can be parsed to int
-splitData.take(1)(0)(5).split('-')(0).replaceAll("^\"|\"$", "").toInt
-splitData.take(1)(0)(5).split('-')(1).replaceAll("^\"|\"$", "").toInt
-splitData.take(1)(0)(5).split('-')(2).replaceAll("^\"|\"$", "").toInt
+splitData.take(1)(0)(2).split('-')(0).replaceAll("^\"|\"$", "").toInt
+splitData.take(1)(0)(2).split('-')(1).replaceAll("^\"|\"$", "").toInt
+splitData.take(1)(0)(2).split('-')(2).replaceAll("^\"|\"$", "").toInt
 
 //Check if precipitation can be parsed to double
-splitData.take(1)(0)(10).replaceAll("^\"|\"$", "").toDouble
+splitData.take(1)(0)(9).replaceAll("^\"|\"$", "").toDouble
 
 //Check if snow values can be parsed to double
-splitData.take(1)(0)(11).replaceAll("^\"|\"$", "").toDouble
+splitData.take(1)(0)(10).replaceAll("^\"|\"$", "").toDouble
 
 //Check if avg temp value can be parsed to int
-splitData.take(1)(0)(13).replaceAll("^\"|\"$", "").toInt
+splitData.take(1)(0)(12).replaceAll("^\"|\"$", "").toInt
 
-val outputData = splitData.map(v => v(0).replaceAll("^\"|\"$", "")+','+v(5).split('-')(0).replaceAll("^\"|\"$", "")+','+ v(5).split('-')(1).replaceAll("^\"|\"$", "")+','+v(5).split('-')(2).replaceAll("^\"|\"$", "")+','+v(10).replaceAll("^\"|\"$", "")+','+v(11).replaceAll("^\"|\"$", "")+','+v(13).replaceAll("^\"|\"$", "") )
+val outputData = splitData.map(v => v(10).replaceAll("^\"|\"$", "")+','+v(11).replaceAll("^\"|\"$", "")+','+v(12).replaceAll("^\"|\"$", "") )
 
 //find max temp
-val maxTemp = outputData.map(line => line.split(',')(6).toInt).max()
+val maxTemp = outputData.map(line => line.split(',')(2).toInt).max()
 
 //find max snow
-val maxSnow = outputData.map(line => line.split(',')(5).toDouble).max()
+val maxSnow = outputData.map(line => line.split(',')(1).toDouble).max()
 
 //find max precipitation
-val maxSnow = outputData.map(line => line.split(',')(4).toDouble).max()
+val maxPrec = outputData.map(line => line.split(',')(0).toDouble).max()
