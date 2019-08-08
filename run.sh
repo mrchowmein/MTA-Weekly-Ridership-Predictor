2cefe8f99e6b5f95d107f0057e2d8724f465eace
Jason <jasonfchan@gmail.com>
2019-08-07 22:06:48 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,6 @@ hdfs dfs -rm -r etlturnstile
 hdfs dfs -rm -r etlweather
 hdfs dfs -rm -r dailyweatheravgforweek
 hdfs dfs -rm -r weeklyfares
-hdfs dfs -rm -r processedDataSet
 hdfs dfs -rm -r finaldataset
 hdfs dfs -rm -r dailyandcumu
 hdfs dfs -rm -r turnstileDailyRatio
@@ -13,5 +12,3 @@ hdfs dfs -rm -r turnstileDailyRatio
 spark-shell -i etlturnstile.scala
 spark-shell -i etlweather.scala
 spark-shell -i compute.scala
-
-
